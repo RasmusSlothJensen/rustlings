a96a5073daf98e339c0594540a313400ef34e414
Rasmus Sloth Jensen <rsj@tat.dev>
2025-10-03 22:07:03 +0200

    work in progress

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -1,4 +1,6 @@
 fn factorial(num: u64) -> u64 {
+    (2..=num).product()
+    // (2..=num).fold(1, |acc, x| acc * x)
     // TODO: Complete this function to return the factorial of `num` which is
     // defined as `1 * 2 * 3 * … * num`.
     // https://en.wikipedia.org/wiki/Factorial
